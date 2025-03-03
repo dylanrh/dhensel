92df8034deaa83e02e9635db7adc094099b5dfc7
Dylan Hensel <dylan@hensel.com>
2025-03-03 12:32:48 -0800

    minor changes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,34 +6,33 @@ import { caseStudiesQuery } from '@root/src/queries/caseStudies';
 import { CaseStudy, Post } from '@root/src/types';
 
 export default async function sitemap() {
-	const blogPosts: Post[] = await sanityClient.fetch(postsQuery());
+	const personalPosts: Post[] = await sanityClient.fetch(postsQuery());
 	const caseStudies: CaseStudy[] = await sanityClient.fetch(caseStudiesQuery());
 
-	const blogRoutes = blogPosts.map(({ slug, publishedAt }) => ({
-		url: `https://jacobherper.com/blog/${slug}`,
+	const personalRoutes = personalPosts.map(({ slug, publishedAt }) => ({
+		url: `https://dhensel.com/personal/${slug}`,
 		lastModified: new Date(publishedAt).toISOString().split('T')[0],
 		priority: 0.65,
 	}));
-	const caseStudiesRoutes = caseStudies.map(({ slug, publishedAt }) => ({
-		url: `https://jacobherper.com/case-studies/${slug}`,
-		lastModified: new Date(publishedAt).toISOString().split('T')[0],
-		priority: 0.65,
-	}));
+	// const caseStudiesRoutes = caseStudies.map(({ slug, publishedAt }) => ({
+	// 	url: `https://jacobherper.com/case-studies/${slug}`,
+	// 	lastModified: new Date(publishedAt).toISOString().split('T')[0],
+	// 	priority: 0.65,
+	// }));
 	const staticRoutes = [
 		'',
 		'/about',
-		'/blog',
-		'/case-studies',
+		'/personal',
+		// '/case-studies',
 		'/contact',
-		'/recruiters',
-		'/uses',
+		// '/uses',
 	].map((route) => {
 		return {
-			url: `https://jacobherper.com${route}`,
+			url: `https://dhensel.com${route}`,
 			lastModified: new Date().toISOString().split('T')[0],
 			priority: route === '' ? 1 : 0.8,
 		};
 	});
 
-	return [...staticRoutes, ...blogRoutes, ...caseStudiesRoutes];
+	return [...staticRoutes, ...personalRoutes];
 }
